9fcbe61ff332a0f775856f8a515a284dfbad78a7
Anandan G <anandang@gmail.com>
2024-10-23 17:48:04 +0530

    :zap: Update profile details

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -259,6 +259,25 @@ $(function() {
 
   bar.animate(.80);
 
+  var bar = new ProgressBar.Line(lineprog8, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    delay: 3200,
+    trailWidth: 1.72,
+    svgStyle: {
+      width: '100%',
+      height: '100%'
+    },
+    step: (state, bar) => {
+      bar.setText(Math.round(bar.value() * 100) + ' %');
+    }
+  });
+
+  bar.animate(.80);
+
+  
+
   var bar = new ProgressBar.Line(lineprog7, {
     strokeWidth: 1.72,
     easing: 'easeInOut',
